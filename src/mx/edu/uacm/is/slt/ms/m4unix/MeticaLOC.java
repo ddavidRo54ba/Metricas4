660da2a9c2c42be845753d1208a07f50ab377251
David Rocha Basilio <david.rocha@estudiante.uacm.edu.mx>
2026-04-10 16:02:42 -0600

    Se agrego clase MetricaLOC

---
+++ src/mx/edu/uacm/is/slt/ms/m4unix/MeticaLOC.java
@@ -0,0 +1,55 @@
+package mx.edu.uacm.is.slt.ms.m4unix;
+
+/**
+ *
+ * @author David R
+ */
+public class MeticaLOC {
+    
+    private int lineasDeCodigo;
+    
+    public MeticaLOC(int lineasDeCodigo){
+        if(lineasDeCodigo < 0){
+            System.out.println("El numero de lineas de codigo no debe ser 0");
+        }
+        this.lineasDeCodigo = lineasDeCodigo;
+    }
+    
+    public int getLineasCodigo(){
+        return lineasDeCodigo;
+    }
+    
+    public String nivelTamanio( ){
+        if (lineasDeCodigo < 500){
+            return "Nivel bajo";
+        }else{
+            if(lineasDeCodigo <= 1500){
+                return "Nivel medio";
+            }else {
+                return "Nivel alto";
+            }
+        }
+    }
+    
+    public String Informacion(){
+        switch(nivelTamanio() ){
+            case "Nivel bajo":
+                return "Sistema pequenio";
+            case "Nivel medio":
+                return "Sistema de tamanio moderado";
+            case "Nivel alto":
+                return "Sistema grande lo que implica mayor complejidad y esfuerzo de mantenimiento";
+            default:
+                return "No definido";
+        }
+    }
+    
+    public String Imprimir(){
+        return "Lineas de codigo:  " + lineasDeCodigo + "\nIndicador tamanio del sistema " + 
+                "\nInterpretacion:  " + nivelTamanio() + "\nDescipcion:  " + Informacion();
+    }
+    
+    
+    
+    
+}
